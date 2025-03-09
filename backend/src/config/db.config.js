19d20c11c20c4ccddfe9bb64ffefa8b7e6586203
Sandi Perdiansah <sandiperdiansah95@gmail.com>
2025-03-10 03:20:55 +0700

    sadasd

--- backend/src/config/db.config.js
+++ backend/src/config/db.config.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 mongoose
    .connect(process.env.MONGODB_URL)
-   .then(() => {})
+   .then(() => {console.log('connect mongodb succesfully');
+   })
    .catch((e) => {
       console.log(e.message);
    });
